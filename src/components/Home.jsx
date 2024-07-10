7971a7988c36738cc5a3baa583e353989b3c31bb
akshaypan <akshaybenjamin7@gmail.com>
2024-07-10 20:35:55 +0530

    Succ

---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        
+        <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div id="carouselExample" class="carousel slide">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://www.urbanplant.in/cdn/shop/products/Primary5copy_22b376aa-154f-45c4-97f2-a2427e9bdcb4.webp?v=1681313864" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://i.rtings.com/assets/pages/1OCszFAw/best-apple-20230425-medium.jpg?format=auto" class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src="https://nestasia.in/cdn/shop/files/Greencoffeemugs_8.jpg?v=1699368644&width=600" class="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br />
+
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Viewallitem.jsx
@@ -0,0 +1,148 @@
+import React from 'react'
+
+const Viewallitem = () => {
+  return (
+    <div>
+
+<div className="container">
+      <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <div className="row g-3">
+           <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+           <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+          </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+            <div class="card">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34s4rx0TuaK7ecE7-sP6frjPWdHL-CqjZvw&s" class="card-img-top" alt="..."/>
+              <div class="card-body">
+                <h5 class="card-title">Plastic pot-white</h5>
+                <p class="card-text">Rs.</p>
+                <a href="#" class="btn btn-primary">Buy now</a>
+              </div>
+            </div>
+
+            </div>
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">SI No.</th>
+      <th scope="col">Item</th>
+      <th scope="col">Item price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Headphone</td>
+      <td>500</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Cup-Plastic</td>
+      <td>80</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Glasses</td>
+      <td>100</td>
+    </tr>
+  </tbody>
+</table>
+
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div> 
+
+    </div>
+  )
+}
+
+export default Viewallitem
